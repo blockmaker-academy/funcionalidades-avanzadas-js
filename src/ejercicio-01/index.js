beae5473e7f57cb556b40f51236320edb030a699
Stamboliysky <jordanspassoff@gmail.com>
2023-11-14 13:46:47 +0100

    Añadir ejercicio-01

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Eejercicio 01: Arrow Fubction en Interaciones
+
+const numbers = [2,3,5,7,10]
+
+const squaredNumbers = numbers.map((numbers) =>numbers * numbers )
+
+console.log(squaredNumbers)
