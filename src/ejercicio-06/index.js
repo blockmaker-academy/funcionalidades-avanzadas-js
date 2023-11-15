f13453d8e9e2048e25c58348b52e1ae944f882b7
Stamboliysky <jordanspassoff@gmail.com>
2023-11-15 08:44:11 +0100

    Añadir ejercicio-06

--- src/ejercicio-06/index.js
+++ src/ejercicio-06/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 06: Destructuring de Arreglos
+
+const fruits =['Manzana','Platano','Naranja','Uva']
+
+const [firstFruit, , ,lastFruits] = fruits
+
+console.log(`Primera fruta: ${firstFruit}`)
+console.log(`Ultima fruta ${lastFruits}`)
