417f2cf60f1bc152ea95f02dd2f6f78dc0d08c68
Stamboliysky <jordanspassoff@gmail.com>
2023-11-15 11:00:41 +0100

    Añadir ejercicio-08

--- src/ejercicio-08/index.js
+++ src/ejercicio-08/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio-08: Rest Parameters en una Función
+
+function sum(... numbers) {
+    return numbers.reduce((total,num) =>
+  total+num, 0  )
+}
+
+const result = sum(1,2,3,4,5)
+
+console.log(`La suma es: ${result}`)
