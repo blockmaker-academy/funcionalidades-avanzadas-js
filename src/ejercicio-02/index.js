3601f458ec452dde54a4240892f49b7289d7227b
A05k-Mikhael-luX <anastasiayanastasia.tarot@gmail.com>
2023-11-15 16:00:05 +0100

    Añadir commit Ejercicio-02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,18 @@
 // Escribe tu código aquí:
+const person = {
+    name:'John',
+    greet: function() {
+        console.log(`hola, soy ${this.name}`)
+    }
+}
+
+person.greet()//jpla, soy John
+//Cambiando a u arrow function
+const person2 = {
+    name:'alice',
+    greet:()=> {
+        console.log(`Hola, soy ${This.name}`)
+    }
+}
+
+persone2.greet //hola, soy undefined ( no cambia el valor de `this^)
